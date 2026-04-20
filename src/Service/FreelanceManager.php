15f01347b8db4f5d19c812fff492a95defa8e07e
Nico <nicolas.desachy@gmail.com>
2026-04-20 10:01:11 +0200

    Feature connector total freelance

--- src/Service/FreelanceManager.php
+++ src/Service/FreelanceManager.php
@@ -1,23 +1,32 @@
 <?php
 namespace App\Service;
 
-use App\Entity\Freelance;
+use App\Entity\FreelanceConso;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 readonly class FreelanceManager
 {
-    public function __construct(private EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface $httpClient,
+    ) {
     }
 
-    public function findTheMostUseFirstname(): string
+    public function findTheMostUseFirstname(): ?string
     {
-        return $this->entityManager->getRepository(Freelance::class)->findTheMostUseFirstname()['firstName'];
+        return $this->entityManager->getRepository(FreelanceConso::class)->findTheMostUseFirstname()['firstName'] ?? null;
     }
 
     // More or less 176k freelances. It would be cool if jean-michel.io had a public API.
     public function getNumberOfFreelancesInJeanMichelWebsiteHomePage(): int
     {
-        return 0;
+        try {
+            $data = $this->httpClient->request('GET', 'https://godzilla.jean-michel.io/metrics')->toArray();
+            return (int) $data['total_global_freelances'];
+        } catch (\Throwable) {
+            //si on est chaud on keep la data dans du cache au cas où on rentre dans ce catch on renvoie la dernière value connue
+            return 0;
+        }
     }
 }
